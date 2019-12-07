ca4f1275554ae59d0f20afae0f06d7ad00f982c7
nitslaszlo <nitslaszlo@gmail.com>
2019-12-07 20:37:34 +0100

    up

--- src/components/__tests__/Basic.spec.ts
+++ src/components/__tests__/Basic.spec.ts
@@ -9,4 +9,7 @@ test("hello world", () => {
 test("fibonacci series", () => {
   const array: number[] = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
   expect(array).toBeInstanceOf(Array);
+  expect(array.every(n => n > 0)).toBe(true);
+  expect(array).toContain(1);
+  expect(array).toContain(2);
 });
