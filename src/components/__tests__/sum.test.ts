a73b79cf380f8f4cacc7308c7d99f449c3adc0ba
László Nits <nitslaszlo@gmail.com>
2019-12-18 11:20:50 +0100

    up

--- src/components/__tests__/sum.test.ts
+++ src/components/__tests__/sum.test.ts
@@ -8,10 +8,10 @@ test("basic2", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test("basic1async", async() => {
+test("basic1async", async () => {
   expect(sum()).toBe(0);
 });
 
-test("basic2async", async() => {
+test("basic2async", async () => {
   expect(sum(66, 34)).toBe(100);
 }, 1000 /* optional timeout */);
